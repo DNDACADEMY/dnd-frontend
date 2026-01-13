1b8b30f91655322fb9ec3f47dd22174b892a223d
Zero-1016 <iolpd42@gmail.com>
2026-01-13 22:03:48 +0900

    chore: vite plugin 추가

--- packages/dds-desktop/vite.config.ts
+++ packages/dds-desktop/vite.config.ts
@@ -1,8 +1,12 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
+    react(),
+    vanillaExtractPlugin(),
     dts({
       include: ['src'],
       exclude: ['**/*.stories.tsx', '**/*.test.tsx', '**/*.test.ts']
