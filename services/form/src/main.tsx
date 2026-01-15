2355a1fe8adfb7dfa60e74f1e9eaa1a153fee7d5
Zero-1016 <iolpd42@gmail.com>
2026-01-15 21:34:00 +0900

    chore: msw 데이터 연결 추가

--- services/form/src/main.tsx
+++ services/form/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { worker } from './mock/browser'
+import { MOCK_ENABLED } from './mock/constants'
 import { Router } from './Router'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>
-)
+async function enableMocking() {
+  if (MOCK_ENABLED) {
+    await worker.start({
+      onUnhandledRequest: 'bypass'
+    })
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root') as HTMLElement).render(
+    <StrictMode>
+      <Router />
+    </StrictMode>
+  )
+})
