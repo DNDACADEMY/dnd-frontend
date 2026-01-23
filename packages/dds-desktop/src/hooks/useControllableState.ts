514996ba5ce5da1938cf5ccf9188401f03c8f01a
Zero-1016 <iolpd42@gmail.com>
2026-01-23 22:54:56 +0900

    docs: useControllableState내 주석 설명추가.

--- packages/dds-desktop/src/hooks/useControllableState.ts
+++ packages/dds-desktop/src/hooks/useControllableState.ts
@@ -6,6 +6,15 @@ interface UseControlStableParams<T, E extends HTMLInputElement | HTMLTextAreaEle
   onChange?: ChangeEventHandler<E>
 }
 
+/**
+ * 제어(Controlled) / 비제어(Uncontrolled) 입력 상태를 공통으로 다루는 훅입니다.
+ * `value`가 있으면 제어 모드, 없으면 `defaultValue`를 기준으로 비제어 모드로 동작해요.
+ * @param params - 제어(Controlled) / 비제어(Uncontrolled) 입력 상태를 관리하는 파라미터입니다.
+ * @param params.value - 제어(Controlled) 모드에서 사용할 값입니다.
+ * @param params.defaultValue - 비제어 모드(Uncontrolled)에서 사용할 초기값입니다.
+ * @param params.onChange - 값이 변경될 때 호출되는 핸들러입니다.
+ * @returns 현재 값과 `onChange` 핸들러를 담은 객체입니다.
+ */
 export function useControllableState<T, E extends HTMLInputElement | HTMLTextAreaElement = HTMLInputElement>(params: UseControlStableParams<T, E>) {
   const { value, defaultValue, onChange } = params
 
