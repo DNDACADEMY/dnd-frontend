f6eac18fbcc64528f37215c8831572c8ff4d1e75
Jihyeong Lee <iolpd42@gmail.com>
2026-02-03 18:20:45 +0900

    chore: storybook 설정 수정 (#42)

--- packages/dds-desktop/.storybook/utils/getAbsolutePath.ts
+++
@@ -1,6 +0,0 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-export const getAbsolutePath = (value: string): string => {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
-}
