3674e1c448d9d848803f88889bc0a39bcb997384
Zero-1016 <iolpd42@gmail.com>
2026-02-03 17:51:23 +0900

    chore: storybook 설정 수정

--- packages/dds-desktop/.storybook/main.ts
+++ packages/dds-desktop/.storybook/main.ts
@@ -1,6 +1,13 @@
-import { getAbsolutePath } from './utils/getAbsolutePath'
+import { createRequire } from 'node:module'
+import { dirname, join } from 'node:path'
 
 import type { StorybookConfig } from '@storybook/react-vite'
+
+const require = createRequire(import.meta.url)
+
+const getAbsolutePath = (value: string): string => {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -10,12 +17,11 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-docs')
   ],
-  framework: getAbsolutePath('@storybook/react-vite'),
-  core: {
-    builder: {
-      name: '@storybook/builder-vite',
-      options: {
-        viteConfigPath: 'vite.config.ts'
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {
+      builder: {
+        viteConfigPath: join(process.cwd(), 'vite.config.ts')
       }
     }
   },
@@ -23,4 +29,5 @@ const config: StorybookConfig = {
     reactDocgen: 'react-docgen-typescript'
   }
 }
+
 export default config
