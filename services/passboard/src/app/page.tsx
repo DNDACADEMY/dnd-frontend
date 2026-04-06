ee99f24438c637316031b6544489cef4c61d6c3c
Zero-1016 <iolpd42@gmail.com>
2026-04-06 14:17:17 +0900

    perf(passboard): 이벤트 데이터 10분 캐싱 적용
    unstable_cache로 checkEvent 결과를 600초 캐싱
    페이지는 force-dynamic 유지로 빌드 타임 API 호출 방지

--- services/passboard/src/app/page.tsx
+++ services/passboard/src/app/page.tsx
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 import { type Metadata } from 'next'
+import { unstable_cache } from 'next/cache'
 
 import { Providers } from './providers'
 import { defaultMetadata } from '../constants/defaultMetadata'
@@ -10,8 +11,10 @@ export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = defaultMetadata
 
+const getCachedEvent = unstable_cache(() => checkEvent(), ['current-event'], { revalidate: 600 })
+
 export default async function Page() {
-  const { name: eventName, id: eventId, resultAnnouncementDateTime, isResultAnnounced } = await checkEvent()
+  const { name: eventName, id: eventId, resultAnnouncementDateTime, isResultAnnounced } = await getCachedEvent()
   const isEventVisible = dayjs(resultAnnouncementDateTime).isBefore(dayjs()) && isResultAnnounced
   const resultAnnouncementDate = new Date(resultAnnouncementDateTime)
 
